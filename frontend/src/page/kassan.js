6b0c667f23ea127975cf09ec894d0d32995c702a
Johan <64014256+Schrodar@users.noreply.github.com>
2022-06-12 11:06:34 +0200

    Update kassan.js

--- frontend/src/page/kassan.js
+++ frontend/src/page/kassan.js
@@ -33,7 +33,7 @@ const Kassan = ({toggle}) => {
             }
         }
         cartCont.push(data);
-        axios.post("http://www.bildsomstod.se/v1/loggin/v1/orders", cartCont)
+        axios.post("http://www.bildsomstod.se/v1/orders", cartCont)
         .then(function(respons) {
             if(respons.status === 201){
                 console.log("dispatichng")
