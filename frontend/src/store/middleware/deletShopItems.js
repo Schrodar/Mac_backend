c83ecca444a3238962387c4ca2a9a5fa50b78a25
johan Schröder <Schrodar@gmail.com>
2022-07-24 14:19:18 +0200

    intreduse Delet shop Items

---
+++ frontend/src/store/middleware/deletShopItems.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import * as actions from "../createAction";
+
+
+const deletShopItems = ({ dispatch }) => next => async action => {
+    
+     // if action is not == authUserBegan  then next action in the middleware array 
+    if (action.type !== actions.deleteShopItemsBegan.type) return next(action);
+    // extracting thees methods from the actions paylode
+    const { url, method, data ,onSuccess, onError} = action.payload;
+   
+    
+    next(action)
+    try {
+        const respons = await axios.request({
+        url,
+        method,
+        data,
+        })
+        if (onSuccess)
+            dispatch({ type: onSuccess, payload: respons.data});
+        
+    } 
+    
+
+    catch (err) {
+       if (onError) dispatch({ type: onError, payload: err })
+    }
+   
+}
+
+export default deletShopItems;
